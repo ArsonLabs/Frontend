a00b6741560cbd9d455bb53a3fb162344a041dc4
creeperita09 <creeperita.09@gmail.com>
2025-09-09 14:36:23 +0200

    Idk

--- register/register.lua
+++ register/register.lua
@@ -7,7 +7,28 @@ gurt.select('#register'):on('click', function()
     local password2 = input.value
     
     if password==password2 then
-        trace.log("Create account")
+        local response = fetch('https://arsonbase.smart.is-a.dev/api/user/register', {
+            method = 'POST',
+            headers = {
+                ['Content-Type'] = 'application/json',
+            },
+            body = JSON.stringify({
+                username = username,
+                password = password,
+            })
+        })
+        if response:ok() then
+            local data = response:json()  -- Parse JSON response
+            local text = response:text()  -- Get as text
+            
+            trace.log('Status: ' .. response.status)
+            trace.log('Status Text: ' .. response.statusText)
+            
+            -- Access headers
+            local contentType = response.headers['content-type']
+        else
+            trace.log('Request failed with status: ' .. response.status)
+        end
     else    
         gurt.select('#notmatch').text = 'Passwords do not match.'
         gurt.select('#notmatch2').text = 'Passwords do not match.'
